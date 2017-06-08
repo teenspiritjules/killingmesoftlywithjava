49e0d2c18123c3879fd6687f6f514bbc433a6fec
Julia Modelewski <Julia Modelewski>
2017-06-09 00:44:50 +0200

    asked herself why -j?? +more friends

--- killingmesoftlywithjava/sorting/bubbles/Bubbles.java
+++ killingmesoftlywithjava/sorting/bubbles/Bubbles.java
@@ -1,37 +1,41 @@
-package bubbles;
+package bubbles; 
 
 public class Bubbles {
 
 	public static void main(String[] args) {
-		
-		int height[] = new int[3];
+
+		int height[] = new int[6];
 			height[0]= 160;
 			height[1]= 175;
 			height[2]= 166;
+			height[3]= 163;
+			height[4]= 180;
+			height[5]= 161;
+
 		int slot = 1;
 		int length = height.length;
-		
+
 		System.out.println("Unsorted:");
 		for(int i = 0; i < length; i++) {
 			System.out.println(height[i]);
 		}
-			
+
 		for (int j = 0; j < length; j++) {
-			for (int i = 0; i < length-1-j; i++) {
+			for (int i = 0; i < length-1; i++) {
 				if (height[i] > height[i+1]) {
 					slot = height[i];
 					height[i] = height[i+1];
 					height[i+1] = slot;
 				}
 			}
-			
+
 		}
 		System.out.println("");
 		System.out.println("Sorted:");
 		for (int i = 0 ; i < length; i++) {
 			System.out.println(""+ height[i] +"");
 			}
-		
+
 	}
 
 }
