a9931bd58784f66072dbd5ee9f40d56fa35fbb8d
Julia Modelewski <Julia Modelewski>
2017-06-09 15:03:16 +0200

    commented sorti

---
+++ killingmesoftlywithjava/sorting/sorti/sortiCommented.java
@@ -0,0 +1,19 @@
+//friendly guy taught me how to create stuff properly. fazit: learned how to properly
+//be lazy
+
+package sorti; //great name by great guy
+
+import java.util.Arrays; //class with methods for messing with arrays
+
+public class Sorti {
+
+	public static void main (String[] args) {
+
+	//i guess this simply..sorts everything and prints it out. as easy as it could
+	//possibly be. algorithm caused headaches gonna cry
+	int[] height = {160, 175, 166, 163, 180, 161, 174};
+	Arrays.sort(height);
+	System.out.println(Arrays.toString(height));
+	}
+
+}
