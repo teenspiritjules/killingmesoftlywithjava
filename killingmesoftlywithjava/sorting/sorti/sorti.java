ba21ce84edef0976de74d3b6811d5058b0c23461
Julia Modelewski <Julia Modelewski>
2017-06-09 14:58:02 +0200

    sorted again

---
+++ killingmesoftlywithjava/sorting/sorti/sorti.java
@@ -0,0 +1,14 @@
+package sorti;
+
+import java.util.Arrays;
+
+public class Sorti {
+
+	public static void main (String[] args) {
+
+	int[] height = {160, 175, 166, 163, 180, 161, 174};
+	Arrays.sort(height);
+	System.out.println(Arrays.toString(height));
+	}
+
+}
